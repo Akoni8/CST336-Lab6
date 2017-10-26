88237fd2dc52294391c3ab1cec0464435ef72723
Akoni <hakmorrison@csumb.edu>
2017-10-26 02:01:28 +0000

    added files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    echo "PHP Connected <br />";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 6</title>
+        <style type="text/css">
+           @import url("styles.css"); 
+        </style>
+    </head>
+    
+    <body>
+        <h1>Authentication/CRUD</h1>
+    </body>
+    
+</html>
